3d2aab2d102997a840a8c864f3325aed1534a455
jojojo0501 <sarukiti0418@outlook.jp>
2021-02-11 19:45:41 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,26 +1,26 @@
-action = {
-  type:'CREATE_EVENTS',
-  title:'2020東京オリンピックのお知らせ',
-  body:'2020年に東京でオリンピックを開催します！つきましては'
-}
+// action = {
+//   type:'CREATE_EVENTS',
+//   title:'2020東京オリンピックのお知らせ',
+//   body:'2020年に東京でオリンピックを開催します！つきましては'
+// }
 
-state = [];
+// state = [];
 
-state = [
-  {id:1,title:'タイトル1',body:'ボディー１'},
-  {id:2,title:'タイトル2',body:'ボディー2'},
-  {id:3,title:'タイトル3',body:'ボディー3'},
-];
+// state = [
+//   {id:1,title:'タイトル1',body:'ボディー１'},
+//   {id:2,title:'タイトル2',body:'ボディー2'},
+//   {id:3,title:'タイトル3',body:'ボディー3'},
+// ];
 
-const events = (state = [],action) => {
-  swich (action.type){
-    case 'CREATE_EVENTS':
+const events = (state=[],action) => {
+  switch (action.type){
+    case 'CREATE_EVENT':
       const event = {title: action.title,body:action.body};
       const length = state.length;
       const id =length ===0 ? 1 : state[length -1].id + 1;
       return [...state,{id:id,...event}];
-    case 'DELETE_EVENTS':
-    case 'DELETE_ALL_EVENTS':
+    case 'DELETE_EVENT':
+    case 'DELETE_ALL_EVENT':
       return [];
     default:
       return state;

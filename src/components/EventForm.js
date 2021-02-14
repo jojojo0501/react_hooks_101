ff11907716cc4728c8a5cbc2198dd78b629e250c
jojojo0501 <sarukiti0418@outlook.jp>
2021-02-14 12:42:59 +0900

    creat operation log in case of event deletion

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -3,7 +3,8 @@ import {
   CREATE_EVENT,
   DELETE_ALL_EVENTS,
   ADD_OPERATION_LOG,
-  DELETE_OPERATION_LOGS
+  DELETE_OPERATION_LOGS,
+  DELETE_ALL_OPERATION_LOGS
 } from '../actions'
 
 import AppContext from '../contexts/AppContext';
@@ -45,6 +46,17 @@ const EventForm = () => {
 
   const unCreatable = body == 0 || title == 0;
 
+  const deleteAllOperationLogs = (e) => {
+    e.preventDefault();
+    const result = window.confirm('全ての操作ログを本当に削除しても良いですか？');
+
+    if (result) {
+      dispatch({
+        type:DELETE_ALL_OPERATION_LOGS
+      });
+    }
+  }
+
   return (
 <>
     <h4>イベント作成フォーム</h4>
@@ -61,6 +73,7 @@ const EventForm = () => {
 
 <button className='btn btn-primary' onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
 <button className='btn btn-danger' onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+<button className='btn btn-danger' onClick={deleteAllOperationLogs} disabled={state.operationLogs.length ===0 }>全ての操作ログを削除する</button>
 </form>
 </>
   )

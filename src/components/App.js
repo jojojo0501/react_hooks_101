9e43136ab5297c9224b166638ec6eefaa17440e5
jojojo0501 <sarukiti0418@outlook.jp>
2021-02-15 09:30:27 +0900

    save state with rocalstorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, {useEffect,useReducer} from 'react';
 import EventForm from './EventForm';
 import Events from './Events';
 import OperationLogs from './OperationLogs';
@@ -6,12 +6,21 @@ import AppContext from '../contexts/AppContext';
 import reducer from '../reducers/index';
 import operationLogs from '../reducers/operationLogs';
 
+const APP_KEY ='appWithRedux';
+
 const App = () => {
-  const initialState = {
+  const appState = localStorage.getItem(APP_KEY);
+  const initialState = appState ? JSON.parse(appState) :{
     events:[],
     operationLogs:[]
   }
+  
   const [state,dispatch] = useReducer(reducer,initialState);
+    
+  useEffect(()=>{
+    const string = JSON.stringify(state);
+    localStorage.setItem(APP_KEY,string)
+  },[state]);
 
   return (
     <AppContext.Provider value={{state,dispatch}}>
